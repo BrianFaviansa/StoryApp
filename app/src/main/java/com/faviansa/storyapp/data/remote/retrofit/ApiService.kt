645aaa2d2e7e1fbcac008a867b92e62ac9b10e99
BrianFaviansa <brianfpd31@gmail.com>
2024-10-24 12:43:06 +0700

    feat: setup retrofit config & response

---
+++ app/src/main/java/com/faviansa/storyapp/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,26 @@
+package com.faviansa.storyapp.data.remote.retrofit
+
+import com.faviansa.storyapp.data.remote.response.LoginResponse
+import com.faviansa.storyapp.data.remote.response.RegisterResponse
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ) : Response<RegisterResponse>
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<LoginResponse>
+
+}
